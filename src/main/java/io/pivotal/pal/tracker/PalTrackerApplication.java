1468819c59b1a304ad666eef4eb6aced12bb9076
rudra <rudranil.sen@wipro.com>
2019-09-10 23:49:07 -0700

    after pull

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,15 +1,10 @@
 package io.pivotal.pal.tracker;
-<<<<<<< HEAD
-=======
-
->>>>>>> e0b7808a438e52fd921dddfb9745820b05b76e02
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PalTrackerApplication {
-<<<<<<< HEAD
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
@@ -21,15 +16,3 @@ public class PalTrackerApplication {
     }
 
 }
-=======
-
-    public static void main(String[] args) {
-        SpringApplication.run(PalTrackerApplication.class, args);
-    }
-@Bean
-    public TimeEntryRepository getTimeEntryRepository(){
-    final TimeEntryRepository inMemoryTimeEntryRepository = new InMemoryTimeEntryRepository();
-    return inMemoryTimeEntryRepository;
-    }
-}
->>>>>>> e0b7808a438e52fd921dddfb9745820b05b76e02
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
 import javax.validation.Valid;
 
 @RestController
@@ -13,16 +12,6 @@ public class WelcomeController {
 
     public WelcomeController(@Value("${welcome.message}")String message){
         this.message=message;
-=======
-@RestController
-public class WelcomeController {
-
-
-    private String message;
-
-    public WelcomeController(@Value("${welcome.message}") String message) {
-        this.message = message;
->>>>>>> e0b7808a438e52fd921dddfb9745820b05b76e02
     }
 
     @GetMapping("/")
